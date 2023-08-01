35e3231b97b523c82ddb7adfcad655b861dd715b
mustafakorkmazz498 <mustafakorkmaz498498@gmail.com>
2023-08-01 13:34:07 +0300

    .. .. . . . . .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-declare var $: any
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ETicaretClient';
+  constructor(private toastrService: CustomToastrService) {
+    toastrService.message('Mustafa','Korkmaz',{
+      messageType : ToastrMessageType.Info,
+      position : ToastrPosition.BottomLeft
+    });
+    toastrService.message('Mustafa','Korkmaz',{
+      messageType : ToastrMessageType.Error,
+      position : ToastrPosition.TopRight
+    });
+    toastrService.message('Mustafa','Korkmaz',{
+      messageType : ToastrMessageType.Success,
+      position : ToastrPosition.TopLeft
+    });
+    toastrService.message('Mustafa','Korkmaz',{
+      messageType : ToastrMessageType.Warning,
+      position : ToastrPosition.BottomRight
+    });
+  }
 }
